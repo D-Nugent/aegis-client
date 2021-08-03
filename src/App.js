7cb1a5c6506a8e088fb93f0afc64420c815bd53b
James King <james_andrew_king@hotmail.co.uk>
2021-08-03 14:19:24 -0700

    created routes and assigned route urls to variables

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import './App.scss';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { 
+  homeURL,
+  aboutURL,
+  contactUsURL,
+  dashboardURL,
+  loginRegisterURL,
+  patronPortalURL   
+} from './utilities/routerConfig';
+import HomePage from './pages/home/Home';
+import AboutPage from './pages/about/About';
+import ContactPage from './pages/contact/Contact';
+import DashboardPage from './pages/dashboard/Dashboard';
+import LoginRegisterPage from './pages/login-register/LoginRegister';
+import PatronPortalPage from './pages/patron-portal/PatronPortal';
 
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
         <h1>AEGIS</h1>
 
         <Switch>
-          
+          <Route exact path={homeURL} component={HomePage} />
+          <Route exact path={aboutURL} component={AboutPage} />
+          <Route exact path={contactUsURL} component={ContactPage} />
+          <Route exact path={dashboardURL} component={DashboardPage} />
+          <Route exact path={loginRegisterURL} component={LoginRegisterPage}/>          
+          <Route exact path={patronPortalURL} component={PatronPortalPage} />
         </Switch>
+
       </BrowserRouter>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/utilities/routerConfig.js
@@ -0,0 +1,6 @@
+export const homeURL = '/';
+export const aboutURL = '/about';
+export const contactUsURL = '/contact-us';
+export const dashboardURL = '/dashboard';
+export const loginRegisterURL = '/login-register';
+export const patronPortalURL = '/patron-portal';
