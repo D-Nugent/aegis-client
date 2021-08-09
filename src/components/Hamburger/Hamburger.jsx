9f2c8d20f70f557d974271604074be1fddddf245
James King <james_andrew_king@hotmail.co.uk>
2021-08-09 15:12:54 -0700

    added visually functional hamburger menu component without links

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -4,15 +4,25 @@ import HamburgerIcon from '../../assets/icons/hamburger-menu.svg';
 
 
 function Hamburger() {
-
     const [ menuVisible, setMenuVisible ] = useState(false);
 
-    console.log(menuVisible);
-
     return (
-        <div onClick={() => setMenuVisible(true)}>
-            <img src={HamburgerIcon} alt="menu" />
-
+        <div className="hamburger">
+            <img src={HamburgerIcon} alt="menu" onClick={() => setMenuVisible(true)}/>
+            {menuVisible &&
+            <aside className="hamburger-menu">
+                <header className="hamburger-menu__header">
+                    <img className="icon" src={null} alt="close menu" onClick={() => setMenuVisible(false)}/>
+                </header>
+                <nav>
+                    <ul>
+                        <li>link</li>
+                        <li>link</li>
+                        <li>link</li>
+                    </ul>
+                </nav>
+            </aside>
+            }
         </div>
     );
 }
