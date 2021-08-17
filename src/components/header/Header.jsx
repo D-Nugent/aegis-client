a137858668763e6b201bf1614ea54748404c731e
James King <james_andrew_king@hotmail.co.uk>
2021-08-16 17:35:23 -0700

    created header @ mobile with pre functionality added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,29 @@
 import './Header.scss';
+import React, { useState } from 'react';
+import AegisLogo from '../../assets/logo/aegis-logo.svg';
+import HamburgerIcon from '../../assets/icons/hamburger-menu.svg';
 
 function Header() {
+    
+    const [ showMenu, setShowMenu ] = useState(true);
+
+    const handleLogin = () => {};
+
+    const handleShowModel = () => setShowMenu(!showMenu);
+
     return (
         <div>
             <div className="header__spacer"></div>
             <header className="header">
                 <div className="header__column">
-                    {/* icon */}
+                    <img className="header__logo" src={AegisLogo} alt="logo" />
                     {/* navbar hidden at mobile */}
                 </div>
                 <div className="header__column">
-                    {/* login */}
-                    {/* hamburger menu at mobile */}
+                    <button className="button-primary header__button" type="button" onClick={handleLogin}>Login</button>
+                    <div className="header__hamburger-container" onClick={handleShowModel}>
+                        <img src={HamburgerIcon} alt="hamburger menu" />
+                    </div>
                 </div>            
             </header>
         </div>
