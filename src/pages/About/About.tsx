721f794c16e9a287c47947a2ab3bb837807f63d9
James King <james_andrew_king@hotmail.co.uk>
2021-08-24 12:51:06 -0700

    created inital HTML structure

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import './About.scss';
 
 function About() {
+
     return (
-        <div>
-            ABOUT            
-        </div>
+        <main>
+            <section>
+                <div>
+                    <h1>About Aegis</h1>
+                    <h2>Mission Statement Goes Here For Impact</h2>
+                </div>
+                <div>
+                    <div>
+                        <h2>Our Purpose</h2>
+                        <p>Lorem Ipsum Text here. A secure platform for reporting, logging and tracking lost and found items. Intended to ease the process for clients and users alike.</p>
+                        <p>Lorem Ipsum Text here. A secure platform for reporting, logging and tracking lost and found items. Intended to ease the process for clients and users alike.</p>
+                    </div>
+                    <svg></svg>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <img src={"#"} alt="Preview of App in Action" />
+                </div>
+                <h2>Why Use Aegis?</h2>
+                <div>
+                    <div>
+                        <p>Lorem Ipsum Text here. A secure platform for reporting, logging and tracking lost and found items. Intended to ease the process for clients and users alike.</p>
+                        <p>Lorem Ipsum Text here. A secure platform for reporting, logging and tracking lost and found items. Intended to ease the process for clients and users alike.</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <svg width="24" height="28" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M16 0H8V2.66667H16V0ZM10.6667 17.3333H13.3333V9.33333H10.6667V17.3333ZM21.3733 8.52L23.2667 6.62667C22.6933 5.94667 22.0667 5.30667 21.3867 4.74667L19.4933 6.64C17.4267 4.98667 14.8267 4 12 4C5.37333 4 0 9.37333 0 16C0 22.6267 5.36 28 12 28C18.64 28 24 22.6267 24 16C24 13.1733 23.0133 10.5733 21.3733 8.52ZM12 25.3333C6.84 25.3333 2.66667 21.16 2.66667 16C2.66667 10.84 6.84 6.66667 12 6.66667C17.16 6.66667 21.3333 10.84 21.3333 16C21.3333 21.16 17.16 25.3333 12 25.3333Z" fill="#FBA919"/>
+                                </svg>
+                                <p>Fast</p>
+                            </li>
+                            <li>
+                                <svg width="24" height="32" viewBox="0 0 24 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M21 10.5H19.5V7.5C19.5 3.36 16.14 0 12 0C7.86 0 4.5 3.36 4.5 7.5V10.5H3C1.35 10.5 0 11.85 0 13.5V28.5C0 30.15 1.35 31.5 3 31.5H21C22.65 31.5 24 30.15 24 28.5V13.5C24 11.85 22.65 10.5 21 10.5ZM12 24C10.35 24 9 22.65 9 21C9 19.35 10.35 18 12 18C13.65 18 15 19.35 15 21C15 22.65 13.65 24 12 24ZM16.65 10.5H7.35V7.5C7.35 4.935 9.435 2.85 12 2.85C14.565 2.85 16.65 4.935 16.65 7.5V10.5Z" fill="#FBA919"/>
+                                </svg>
+                                <p>Secure</p>
+                                
+                            </li>
+                            <li>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M22.6667 16.6667C21 16.6667 19.4 16.4 17.9067 15.9067C17.44 15.76 16.92 15.8667 16.5467 16.2267L13.6133 19.16C9.84 17.24 6.74667 14.16 4.82667 10.3733L7.76 7.42667C8.13333 7.08 8.24 6.56 8.09333 6.09333C7.6 4.6 7.33333 3 7.33333 1.33333C7.33333 0.6 6.73333 0 6 0H1.33333C0.6 0 0 0.6 0 1.33333C0 13.8533 10.1467 24 22.6667 24C23.4 24 24 23.4 24 22.6667V18C24 17.2667 23.4 16.6667 22.6667 16.6667ZM21.3333 12H24C24 5.37333 18.6267 0 12 0V2.66667C17.16 2.66667 21.3333 6.84 21.3333 12ZM16 12H18.6667C18.6667 8.32 15.68 5.33333 12 5.33333V8C14.2133 8 16 9.78667 16 12Z" fill="#FBA919"/>
+                                </svg>
+                                <p>Supported</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>    
+            <section>
+                <h2>The Team Behind It All</h2>
+                <p>Lorem Ipsum Text here. A secure platform for reporting, logging and tracking lost and found items. Intended to ease the process for clients and users alike.</p>
+                <p>Lorem Ipsum Text here. A secure platform for reporting, logging and tracking lost and found items. Intended to ease the process for clients and users alike.</p>
+                <div>
+                    <img src={"#"} alt="David Nugent" />
+                    <img src={"#"} alt="James King" />
+                </div>
+            </section>
+            {/* Return to Top Button */}
+        </main>
     );
+
 }
 
 export default About;
