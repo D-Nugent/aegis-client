d03653be3dca9190ab45e49aa67e1d798bad49c3
James King <james_andrew_king@hotmail.co.uk>
2021-08-31 15:49:15 -0700

    created temporary hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,21 +9,17 @@ type Props = {
 function Hero({ classes, children } : Props) {
 
     return (
-        <section className={`hero${classes ? ` ${classes}` : ""}`}>
-            { children }
-            {/* <svg className="hero__svg-curve-clip-path" viewBox="0 0 320 167">
-                <defs>
-                    <clipPath id="svgPathTopCurve" clipPathUnits="objectBoundingBox">
-                        <path d="M1 0C0.531574 0.0332416 0.571931 0.606902 0.439015 0.764107C0.310989 0.915528 0.166843 0.996916 0.0171744 0.999914H1V0Z" fill="#143058"/>
-                    </clipPath>
-                </defs>
-            </svg>
-            <svg className="hero__svg-curve" preserveAspectRatio="none" viewBox="0 0 320 167" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <section className="hero">
+            <div className={classes}>
+                { children }
+            </div>
+            <svg className="svg-hero__curve" preserveAspectRatio="none" viewBox="0 0 320 167" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M320 1C170.104 6.51009 183.018 101.599 140.485 127.658C99.5164 152.757 53.3898 166.248 5.49582 166.745H320V1Z" fill="#143058"/>
                 <path d="M5.49582 166.745C53.3898 166.248 99.5164 152.757 140.485 127.658C183.018 101.599 170.104 6.51009 320 1V166.745H5.49582ZM5.49582 166.745H0C1.8344 166.764 3.6664 166.764 5.49582 166.745Z" stroke="#143058" strokeWidth="0.250391"/>
-            </svg> */}
+            </svg>
         </section>
     );
+
 }
 
 export default Hero;
